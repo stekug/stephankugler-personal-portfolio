fae8d165b8acc35ca47bb275aa9e6e3b7d174c01
Stephan Kugler <stephan.kugler@adesso.de>
2025-02-24 10:05:01 +0100

    position pic logo, hide scrollbar

--- app/_components/ProfileLogo.tsx
+++ app/_components/ProfileLogo.tsx
@@ -4,15 +4,17 @@ import profilePic from '@/public/sk-profile-1.jpeg';
 
 export default function ProfileLogo() {
   return (
-    <Link href="/" className="flex items-center gap-4 z-10 rounded-full">
-      <Image
-        className="rounded-full"
-        src={profilePic}
-        alt="Profile picture of Stephan Kugler"
-        height="300"
-        width="300"
-        quality={80}
-      />
-    </Link>
+    <div className="gap-10">
+      <Link href="/" className="flex items-center z-10 rounded-full mt-2 py-5">
+        <Image
+          className="rounded-full"
+          src={profilePic}
+          alt="Profile picture of Stephan Kugler"
+          height="300"
+          width="300"
+          quality={80}
+        />
+      </Link>
+    </div>
   );
 }
