c9dcfdd6ed9d329643a3e05da5bbe7d79052b68b
Stephan Kugler <stephan.kugler@adesso.de>
2025-02-24 11:22:05 +0100

    style navigation hover effect

--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -5,16 +5,36 @@ export default function Navigation() {
     <nav className="flex overflow-visible w-full py-5 fixed top-0 bg-primary-8 backdrop-blur-sm">
       <ul className="flex justify-center w-full gap-12 font-extrabold text-2xl">
         <li>
-          <Link href="/">Home</Link>
+          <Link
+            href="/"
+            className="hover:text-accentGreen-1 hover:border-t-2 border-accentGreen-1 transition-colors"
+          >
+            Home
+          </Link>
         </li>
         <li>
-          <Link href="/">About</Link>
+          <Link
+            href="/"
+            className="hover:text-accentGreen-1 hover:border-t-2 border-accentGreen-1 transition-colors"
+          >
+            About
+          </Link>
         </li>
         <li>
-          <Link href="/">My Work</Link>
+          <Link
+            href="/"
+            className="hover:text-accentGreen-1 hover:border-t-2 border-accentGreen-1 transition-colors"
+          >
+            My Work
+          </Link>
         </li>
         <li>
-          <Link href="/">Contact</Link>
+          <Link
+            href="/"
+            className="hover:text-accentGreen-1 hover:border-t-2 border-accentGreen-1 transition-colors"
+          >
+            Contact
+          </Link>
         </li>
       </ul>
     </nav>
