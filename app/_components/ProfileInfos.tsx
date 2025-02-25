b0b243b306bb7ccfc2db7673c53df53cc46e39ea
Stephan Kugler <150922232+stekug@users.noreply.github.com>
2025-02-25 09:09:20 +0100

    Feat: Design base (#3)
    change font and padding size for mobile version
    move profile text to ProfileText component

--- app/_components/ProfileInfos.tsx
+++ app/_components/ProfileInfos.tsx
@@ -1,19 +1,11 @@
 import ProfileLogo from './ProfileLogo';
+import ProfileText from './ProfileText';
 
 export default function ProfileInfos() {
   return (
-    <section className="flex flex-col justify-center items-center py-20">
+    <section className="flex flex-col justify-center items-center pt-24">
       <ProfileLogo />
-      <p className="max-w-md text-3xl text-center font-normal">
-        Hello, my name is{' '}
-        <span className="font-black text-accentGreen-1 uppercase">
-          Stephan Kugler.
-        </span>{' '}
-        I <span className="font-black text-accentPink-1">create</span>,{' '}
-        <span className="font-black text-accentPink-1">code</span> and{' '}
-        <span className="font-black text-accentPink-1">enjoy</span> every{' '}
-        <span className="font-black text-accentPink-1">bit</span> of it!
-      </p>
+      <ProfileText />
     </section>
   );
 }
---
+++ app/_components/ProfileText.tsx
@@ -0,0 +1,14 @@
+export default function ProfileText() {
+  return (
+    <p className="max-w-md text-4xl text-center font-normal px-3">
+      Hello, my name is{' '}
+      <span className="font-black text-accentGreen-1 uppercase">
+        Stephan Kugler.
+      </span>{' '}
+      I <span className="font-black text-accentPink-1">create</span>,{' '}
+      <span className="font-black text-accentPink-1">code</span> and{' '}
+      <span className="font-black text-accentPink-1">enjoy</span> every{' '}
+      <span className="font-black text-accentPink-1">bit</span> of it!
+    </p>
+  );
+}
