e2009ecf33e47da756061b0cb42a666fee7c034d
Stephan Kugler <stephan.kugler@adesso.de>
2025-02-28 10:01:08 +0100

    cleanup ProfileSections

--- app/_components/ProfileLogo.tsx
+++ app/_components/ProfileLogo.tsx
@@ -1,20 +1,50 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import profilePic from '@/public/sk-profile-1.jpeg';
+import Github from './Github';
+import Linkedin from './Linkedin';
 
 export default function ProfileLogo() {
   return (
-    <div className="gap-10">
-      <Link href="/" className="flex items-center z-10 rounded-full mt-2 py-5">
+    <section className="mt-10">
+      <Link
+        href="/"
+        className="flex flex-col items-center z-10 rounded-full py-5"
+      >
         <Image
           className="rounded-full shadow-[0px_0px_60px_rgba(50,60,90,0.5)]"
           src={profilePic}
           alt="Profile picture of Stephan Kugler"
-          height="260"
-          width="260"
+          height="350"
+          width="350"
           quality={80}
         />
       </Link>
-    </div>
+
+      <div>
+        <p className="max-w-md mx-auto text-4xl text-center font-normal py-5">
+          Hello, my name is{' '}
+          <span className="font-black text-accentGreen-1 uppercase">
+            Stephan Kugler.
+          </span>{' '}
+          I <span className="font-black text-accentPink-1">create</span>,{' '}
+          <span className="font-black text-accentPink-1">code</span> and{' '}
+          <span className="font-black text-accentPink-1">enjoy</span> every{' '}
+          <span className="font-black text-accentPink-1">bit</span> of it!
+        </p>
+      </div>
+
+      <div className="flex justify-center py-5 gap-10">
+        <Link href="https://github.com/stekug" target="_blank">
+          <Github className="fill-accentGreen-1 hover:fill-accentPink-1 hover:scale-110 transition-all duration-500 w-12 h-12" />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/stephan-kugler-149163115/"
+          target="_blank"
+        >
+          <Linkedin className="fill-accentGreen-1 hover:fill-accentPink-1 hover:scale-110 transition-all duration-500 w-12 h-12" />
+        </Link>
+      </div>
+    </section>
   );
 }
--- app/_components/SectionModul.tsx
+++ app/_components/SectionModul.tsx
@@ -7,7 +7,7 @@ interface Props extends PropsWithChildren {
 export default function SectionModul({ children, customBg }: Props) {
   return (
     <section
-      className={`flex flex-col justify-center items-center px-10 pt-24 ${customBg}`}
+      className={`flex flex-col justify-center items-center p-10 ${customBg}`}
     >
       {children}
     </section>
