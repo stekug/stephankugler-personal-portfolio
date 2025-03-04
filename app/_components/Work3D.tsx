6be6b675b896798cb2e8fb1f6a126f92685f4e31
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-04 13:55:39 +0100

    add Button functionality to show or hide 3d projects

--- app/_components/Work3D.tsx
+++ app/_components/Work3D.tsx
@@ -1,6 +1,11 @@
+"use client";
+
+import { useState } from "react";
 import Projects3D from "./Projects3D";
 
 export default function Work3D() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <section className="mb-8 w-full max-w-2xl">
       <span className="mb-4 block text-xl font-black uppercase tracking-wide text-accentPink-1">
@@ -11,10 +16,13 @@ export default function Work3D() {
         animations. If you are interested to see a selection of my
         &quot;old&quot; work, you are more than welcome!
       </p>
-      <button className="mt-2 rounded-md bg-accentGreen-1 px-1 font-bold text-primary-2 hover:bg-accentPink-1">
-        Show 3D projects
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="mt-4 rounded-md bg-accentGreen-1 px-1 font-bold text-primary-2 hover:bg-accentPink-1"
+      >
+        {isOpen ? "Hide 3D projects" : "Show 3D projects"}
       </button>
-      <Projects3D />
+      {isOpen && <Projects3D />}
     </section>
   );
 }
