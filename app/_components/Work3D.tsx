585799805d18843d1f64f472ccd2b1974497672f
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-04 14:35:13 +0100

    change Work3D welcome text

--- app/_components/Work3D.tsx
+++ app/_components/Work3D.tsx
@@ -11,12 +11,12 @@ export default function Work3D() {
   return (
     <section className="mb-8 w-full max-w-2xl">
       <span className="mb-4 block text-xl font-black uppercase tracking-wide text-accentPink-1">
-        My &quot;old&quot; 3D Work
+        Throwback: My 3D Creations
       </span>
       <p>
-        I mentioned that my background includes 3D animation. If you&apos;re
-        interested in exploring my earlier work, just click the button on the
-        website to see the projects.
+        I mentioned that my background includes design and 3D animation. If
+        you&apos;re interested in exploring my earlier work, just click the
+        button on the website to see the projects.
       </p>
       <ProjectButton onClick={() => setIsOpen(!isOpen)}>
         {isOpen ? "Hide 3D projects" : "Show 3D projects"}
