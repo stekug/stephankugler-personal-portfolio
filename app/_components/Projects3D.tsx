e028850e4283661ee0f69e3dab746e55e75a5259
Stephan Kugler <stephan.kugler@adesso.de>
2025-03-04 09:46:16 +0100

    add Suspense component

--- app/_components/Projects3D.tsx
+++ app/_components/Projects3D.tsx
@@ -1,12 +1,13 @@
 import IframeVideo from "./IframeVideo";
 import { Suspense } from "react";
+import VideoSkeleton from "./VideoSkeleton";
 
 export default function Projects3D() {
   return (
     <div>
       <p>3d Projects</p>
       <div className="">
-        <Suspense fallback={<p>Loading Video</p>}>
+        <Suspense fallback={<VideoSkeleton />}>
           <IframeVideo />
         </Suspense>
       </div>
